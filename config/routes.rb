ad21c26eee92d4f584c59f29aede5b4514db58c3
Khanh <khanh.pb25@gmail.com>
2017-11-14 01:41:25 +0700

    Fix Http Method In Omniauth Route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: "json" } do
     devise_for :users, skip: :registrations, controllers: {sessions: "api/sessions"}
 
-    get 'auth/omniauths', to: 'omniauths#create'
+    post 'auth/omniauths', to: 'omniauths#create'
     delete 'auth/omniauths', to: 'omniauths#destroy'
 
     resources :users, except: %i[new edit]
