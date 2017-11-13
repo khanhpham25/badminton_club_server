4f36e537897b64f8aa76a1dcdaf082208475f4a3
Khanh <khanh.pb25@gmail.com>
2017-11-14 01:28:19 +0700

    Add Authenticatio For Facebook

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: "json" } do
     devise_for :users, skip: :registrations, controllers: {sessions: "api/sessions"}
 
+    get 'auth/omniauths', to: 'omniauths#create'
+    delete 'auth/omniauths', to: 'omniauths#destroy'
+
     resources :users, except: %i[new edit]
   end
 end
