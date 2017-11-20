f07ef802ec6bd810d71cd850191f4dea15bedc75
Khanh <khanh.pb25@gmail.com>
2017-11-20 15:39:39 +0700

    Fix Reset Token Return

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,8 +30,7 @@ class User < ApplicationRecord
   end
 
   def create_reset_digest
-    self.reset_token = User.new_token
-    update_attributes reset_digest: User.digest(reset_token),
+    update_attributes reset_digest: User.new_token,
                       reset_sent_at: Time.zone.now
   end
 
