2931a0115cc65d9b95e9b125b479f1ee3a438fec
Khanh <khanh.pb25@gmail.com>
2017-12-14 12:31:01 +0700

    Manager User Admin

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,3 +1,7 @@
 setTimeout(function() {
   $('.alert').fadeOut('slow');
 }, 5000);
+
+$(document).on('turbolinks:load', function() {
+  $('#user-tables').DataTable();
+});
--- config/routes.rb
+++ config/routes.rb
@@ -21,4 +21,6 @@ Rails.application.routes.draw do
 
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+
+  resources :users
 end
