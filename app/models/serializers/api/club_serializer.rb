78a7608b939379a0a6def4d589955f2b13aebbc3
Khanh <khanh.pb25@gmail.com>
2017-12-26 16:20:04 +0700

    Fix Avatar Return Of Club

--- app/models/serializers/api/club_serializer.rb
+++ app/models/serializers/api/club_serializer.rb
@@ -5,6 +5,11 @@ module Serializers
             :all_members_count, :is_recruiting, :allow_friendly_match,
             :location, :latitude, :longitude
 
+      def avatar
+        return unless object.avatar?
+        Hash[:url, object.avatar.url]
+      end
+
       def all_members_count
         object.members.count + object.owners.count
       end
